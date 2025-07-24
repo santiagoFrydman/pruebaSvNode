3b98c3ac8ce116cf436d8dacba9cef27c2e7c50d
Frydman <santiago.frydman@hospitalbonaparte.gob.ar>
2025-07-24 13:25:33 -0300

    Cambio de puerto

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8000,
   MODO_PERSISTENCIA: process.env.MODO_PERSISTENCIA || '',
   SESSION_SECRET: process.env.SESSION_SECRET || 'clave_super_secreta'
 }
