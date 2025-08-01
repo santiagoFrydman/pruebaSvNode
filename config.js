8ffc6c77cb3bb3da9368bee3e5cac6f01041ce26
Frydman <santiago.frydman@hospitalbonaparte.gob.ar>
2025-08-01 15:09:55 -0300

    Cambiada la ruta del path para railway

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ dotenv.config()
 export default {
   PORT: process.env.PORT || 8000,
   SESSION_SECRET: process.env.SESSION_SECRET || 'clave_super_secreta',
-  DB_PATH: process.env.DB_PATH || './database.db',
+  DB_PATH: process.env.DB_PATH || '/data/database.db',
   DB_PATH_TEST: process.env.DB_PATH_TEST || './database-test.db'
 }
